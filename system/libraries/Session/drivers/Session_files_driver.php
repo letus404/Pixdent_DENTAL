ccaf267d6f4bf84ea73ce1d1b2fac7a483c77638
Mariano Quinteros <and1_iverson@live.com.ar>
2024-07-29 11:29:29 -0500

    corrección funcion open transformada a booleana - primer error

--- system/libraries/Session/drivers/Session_files_driver.php
+++ system/libraries/Session/drivers/Session_files_driver.php
@@ -129,7 +129,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 * @param	string	$name		Session cookie name
 	 * @return	bool
 	 */
-	public function open($save_path, $name)
+	public function open($save_path, $name) : bool
 	{
 		if ( ! is_dir($save_path))
 		{
